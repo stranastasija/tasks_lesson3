76594ff9d380fad8ac1ac166c396139621f77435
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-06 00:39:13 +0300

    add task3 - большее из чисел

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Даны два числа. Показать большее и меньшее.
+
+double numberA = new Random().Next(0, 100);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(0, 100);
+Console.WriteLine(numberB);
+
+if (numberA < numberB)
+{
+    Console.WriteLine(numberB + " - больше, " + numberA + " - меньше");
+}
+if (numberA > numberB)
+{
+    Console.WriteLine(numberA + " - больше, " + numberB + " - меньше");
+}
+if (numberA == numberB)
+{
+    Console.WriteLine("Числа равны");
+}
