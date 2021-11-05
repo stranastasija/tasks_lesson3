9d5c549c6414fed1e10b7bf4ed134f564a1ce18a
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-06 00:12:38 +0300

    add task2 - два числа, квадрат одного из них - второе число

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго.
+
+double numberA = 25; //new Random().Next(0, 100);
+Console.WriteLine(numberA);
+double numberB = 5; //new Random().Next(0, 100);
+Console.WriteLine(numberB);
+//При рандоме вероятность попасть в квадрат очень мала.
+if (numberA == numberB*numberB)
+{
+    Console.WriteLine(numberA + " - квадрат " + numberB);
+}
+if (numberA != numberB*numberB)
+{
+    Console.WriteLine("Числа не связаны");
+}
