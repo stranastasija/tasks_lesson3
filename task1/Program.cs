c12d1dda04adbe9051aaab7b5f0ea99e13acfea0
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-05 23:55:39 +0300

    add task1 - выведение квадрата числа

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вывести квадрат числа
+
+double number = new Random().Next(0, 100);
+//Возникла проблема с тем, что изначально писала код, где число вводил пользователь. 
+//Но нигде не могу найти, как преобразовать string в int/double. 
+//Так что этот вариант пока в работе.
+
+Console.WriteLine(number);
+double result = number * number;
+Console.Write("Квадрат числа = ");
+Console.WriteLine(result);
