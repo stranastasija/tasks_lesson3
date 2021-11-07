49745e879c2209750e9eac810d92291546ab2648
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-07 23:08:06 +0300

    add task4 - дни недели

---
+++ task4/Program.cs
@@ -0,0 +1,35 @@
+﻿// По заданному номеру дня недели вывести его название.
+
+//string day = Console.ReadLine("Введите номер дня недели");
+
+int day = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер дня недели");
+
+if (day ==1)
+{
+    Console.WriteLine("Monday");
+}
+if (day ==2)
+{
+    Console.WriteLine("Tuesday");
+}
+if (day ==3)
+{
+    Console.WriteLine("Wednesday");
+}
+if (day ==4)
+{
+    Console.WriteLine("Thursday");
+}
+if (day ==5)
+{
+    Console.WriteLine("Friday");
+}
+if (day ==6)
+{
+    Console.WriteLine("Saturday");
+}
+if (day ==7)
+{
+    Console.WriteLine("Sunday");
+}
