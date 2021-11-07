64cc9b16d85de42fa044de40c39c69b1df8f46d6
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-08 00:22:13 +0300

    new version task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -2,34 +2,53 @@
 
 //string day = Console.ReadLine("Введите номер дня недели");
 
-int day = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите номер дня недели");
+int day = Convert.ToInt32(Console.ReadLine());
 
-if (day ==1)
+if (day == 1)
 {
     Console.WriteLine("Monday");
+    return;
 }
-if (day ==2)
+if (day == 2)
 {
     Console.WriteLine("Tuesday");
+    return;
 }
-if (day ==3)
+if (day == 3)
 {
     Console.WriteLine("Wednesday");
+    return;
 }
-if (day ==4)
+if (day == 4)
 {
     Console.WriteLine("Thursday");
+    return;
 }
-if (day ==5)
+if (day == 5)
 {
     Console.WriteLine("Friday");
+    return;
 }
-if (day ==6)
+if (day == 6)
 {
     Console.WriteLine("Saturday");
+    return;
 }
-if (day ==7)
+if (day == 7)
 {
     Console.WriteLine("Sunday");
+    return;
+}
+
+
+if (day < 8)
+{
+    Console.WriteLine("Такого дня недели не существует");
+    return;
+}
+if (day > 0)
+{
+    Console.WriteLine("Такого дня недели не существует");
+    return;
 }
