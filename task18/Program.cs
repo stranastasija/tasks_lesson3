1ce8ca151d9e88a705f73f2e5409b42a9c24cf93
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-09 00:19:05 +0300

    add task18 - истинность утверждения

---
+++ task18/Program.cs
@@ -0,0 +1,11 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y 
+
+bool text (bool X, bool Y)
+{
+    return (!(X || Y) == (!X && !Y));
+}
+Console.WriteLine(text(true, true));
+Console.WriteLine(text(true, false));
+Console.WriteLine(text(false, true));
+Console.WriteLine(text(false, false));
+//Показываем, что все варианты дают истину.
