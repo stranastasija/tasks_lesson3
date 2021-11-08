498dd968e315c167e7579121b5153b3290a381e9
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-09 02:30:40 +0300

    add task21 - палиндром

---
+++ task21/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа проверяет пятизначное число на палиндромом    //01234 i
+                                                           //12321
+bool palindrom (int a)
+{
+    if (a < 10000 || a > 99999)
+    {
+        Console.WriteLine("Число не пятизначное");
+    }
+    string number = Convert.ToString(a);
+    return (number[0] == number[4]) && (number[1] == number[3]);
+    
+}
+Console.WriteLine($"{(palindrom(12321) ? "Число палиндром" : "Число не палиндром")}");
