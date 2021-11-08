c0b25339d6537c137fdea71f8d4001d6293512cd
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-09 02:15:29 +0300

    add task20 - новая форма switch

---
+++ task20/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Введите цифру от 1 до 4, чтобы узнать диапозон четверти окружности");
+int number = int.Parse(Console.ReadLine());
+
+switch (number)
+{
+    case 1 : Console.WriteLine("Х от 0 до +бесконечности, У от 0 до +бесконечности"); break;
+    case 2 : Console.WriteLine("Х от -бесконечности до 0, У от 0 до +бесконечности"); break;
+    case 3 : Console.WriteLine("Х от -бесконечности до 0, У от -бесконечности до 0"); break;
+    case 4 : Console.WriteLine("Х от 0 до +бесконечности, У от -бесконечности до 0"); break;
+    default : Console.WriteLine(" - не входит в систему координат"); break;
+}
