536f21c0e88beb7195a5e4476bf9d2e66e19cbfd
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-12 01:53:58 +0300

    add new version

--- task12/Program.cs
+++ task12/Program.cs
@@ -1,13 +1,27 @@
 ﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
 
-Console.WriteLine ("Введите число от 10 до 99");
-string N = Console.ReadLine();
+//Console.WriteLine ("Введите число от 10 до 99");
+//string N = Console.ReadLine();
 
-if (N[0]>N[1])
+//if (N[0]>N[1])
+//{
+//    Console.WriteLine(N[0]);
+//}
+//else
+//{
+//    Console.WriteLine(N[1]);
+//}
+
+int number = new Random().Next(10, 100);
+Console.WriteLine(number);
+
+char max (int number)
 {
-    Console.WriteLine(N[0]);
+    string N = Convert.ToString(number);
+    char first = N[0];
+    char second = N[1];
+    if (first > second) return first;
+    else return second;
 }
-else
-{
-    Console.WriteLine(N[1]);
-}
+char result = max(number);
+Console.WriteLine(result);
