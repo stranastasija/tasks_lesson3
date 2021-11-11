e4bfc757dfc40e2e5a565f971d680b6e9607d9c9
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-11 22:59:28 +0300

    add task7 - четное число

---
+++ task7/Program.cs
@@ -0,0 +1,8 @@
+﻿// Выяснить, является ли число четным
+
+bool number (int a)
+{
+    string number = Convert.ToString(a);
+    return (a %2==0);
+}
+Console.WriteLine($"{(number(9) ? " число четное" : " число нечетное")}");
