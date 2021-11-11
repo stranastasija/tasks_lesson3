f134276c03bc16082501689199ecad5547f44111
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-11 20:27:16 +0300

    add task5 - максимальное из трех чисел

---
+++ task5/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти максимальное из трех чисел
+
+int max (int a, int b, int c)
+{
+    int max = a;
+    return (a > b && a > c) ? max = a : ((b > a && b > c) ? max = b : max = c);
+}
+Console.WriteLine(max(2, 5, 10));
