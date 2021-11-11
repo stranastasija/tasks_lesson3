ab878793fdfeb9b466d173a359c571543e4acf80
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-12 01:16:35 +0300

    add task10 - последняя цифра

---
+++ task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать последнюю цифру трехзначного числа
+
+int tree (int a)
+{
+    if (a < 100 || a > 1000)
+    {
+        Console.WriteLine("Число не трехзначное");
+    }
+    
+    return (a%10);
+}
+Console.WriteLine(tree (367));
