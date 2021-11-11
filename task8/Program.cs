a18586bccc0a18b6657515066a2b8e9e9aa57180
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-11 23:52:23 +0300

    add task8 - от -N до N

---
+++ task8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать числа от -N до N
+
+int numbers (int N)
+{
+   int count = -N;
+   while (count <= N)
+   {
+       Console.WriteLine(count);
+       count++;
+   } 
+   return ('N'); //почему выводится 78...
+}
+Console.Write (numbers(3));
