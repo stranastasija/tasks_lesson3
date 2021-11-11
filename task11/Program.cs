bca75d13eaaf84fa196f85a56775dbad23b7a69f
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-12 01:27:40 +0300

    add task11 - вторая цифра

---
+++ task11/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать вторую цифру трехзначного числа
+
+int second (int a)
+{
+    if (a < 100 || a > 1000)
+    {
+        Console.WriteLine("Число не трехзначное");
+        
+    }
+    int N = 0;
+    N = a/10;
+
+    return (N%10);
+}
+Console.WriteLine(second (932));
