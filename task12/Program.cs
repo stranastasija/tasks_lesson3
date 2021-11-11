1b8eea31034bd298e40b93bb0c49b14ee6052ef6
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-12 01:41:59 +0300

    add task12 - наибольшая цифра

---
+++ task12/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+Console.WriteLine ("Введите число от 10 до 99");
+string N = Console.ReadLine();
+
+if (N[0]>N[1])
+{
+    Console.WriteLine(N[0]);
+}
+else
+{
+    Console.WriteLine(N[1]);
+}
