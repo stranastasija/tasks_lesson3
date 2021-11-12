06dc3ca51bc495aa39d29c05ca4835e1a8194c96
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-12 18:58:15 +0300

    add task13 - третья цифра в числе

---
+++ task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти третью цифру числа или сообщить, что ее нет
+
+// Вывод третьей цифры слева направо
+
+int a = new Random().Next (0, 1000000);
+
+Console.Write(a);
+Console.WriteLine(" - заданное число");
+
+if (a<=99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+string i = Convert.ToString(a);
+
+Console.Write(i[2]);
+Console.WriteLine(" - третья цифра");
