709df8d99d6a0981e49bc8e2a71d33d58097560c
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-12 19:16:07 +0300

    add task14 - кратность заданному числу

---
+++ task14/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток
+// Заданное = 7
+
+
+double N = 48;
+Console.Write(N);
+
+double ost = N%7;
+if (ost == 0)
+{
+    Console.WriteLine(" - число кратно 7");
+}
+else 
+{
+    Console.WriteLine(" - число не кратно, остаток = " + ost);
+}
