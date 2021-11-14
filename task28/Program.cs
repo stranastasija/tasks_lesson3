ac914c393ef0aa4468190b61e9ce1c75b1f629a8
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-15 00:49:19 +0300

    add task28 - сумма цифр в числе

---
+++ task28/Program.cs
@@ -0,0 +1,13 @@
+﻿// Подсчитать сумму цифр в числе
+
+int sum (int a)
+{
+    int result = 0;
+    while (a > 0)
+    {
+        result += a % 10;
+        a = a/10;
+    }
+    return result;
+}
+Console.WriteLine (sum (12395));
