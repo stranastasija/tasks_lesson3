33a280923ee02d2871f251f0809f908724ac46a4
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-15 01:03:30 +0300

    add task29 - произведение чисел

---
+++ task29/Program.cs
@@ -0,0 +1,67 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+int N = 10;
+
+int count = 1;
+while (count <=N)
+{
+    Console.WriteLine("2*2= " + count*2);
+    count++;
+}
+Console.WriteLine(" ");
+
+int count1 = 1;
+while (count1 <=N)
+{
+    Console.WriteLine("2*3= " + count1*3);
+    count1++;
+}
+Console.WriteLine(" ");
+
+int count2 = 1;
+while (count2 <=N)
+{
+    Console.WriteLine("2*4= " + count2*4);
+    count2++;
+}
+Console.WriteLine(" ");
+
+int count3 = 1;
+while (count3 <=N)
+{
+    Console.WriteLine("2*5= " + count3*5);
+    count3++;
+}
+Console.WriteLine(" ");
+
+int count4 = 1;
+while (count4 <=N)
+{
+    Console.WriteLine("2*6= " + count4*6);
+    count4++;
+}
+Console.WriteLine(" ");
+
+int count5 = 1;
+while (count5 <=N)
+{
+    Console.WriteLine("2*7= " + count5*7);
+    count5++;
+}
+Console.WriteLine(" ");
+
+int count6 = 1;
+while (count6 <=N)
+{
+    Console.WriteLine("2*8= " + count6*8);
+    count6++;
+}
+Console.WriteLine(" ");
+
+int count7 = 1;
+while (count7 <=N)
+{
+    Console.WriteLine("2*9= " + count7*9);
+    count7++;
+}
+Console.WriteLine(" ");
