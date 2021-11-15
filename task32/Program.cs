e385c2dca9ce860692a08ef1fdcba7dad235dc40
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-15 19:01:52 +0300

    add task32 - массив из 0 и 1

---
+++ task32/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран 
+
+int [] A = new int [8];
+int index = 0;
+
+while (index < 8)
+{
+    A [index] = new Random().Next(-1, 2); //если поставить (-1, 1), то выводит только -1, 0
+    index++;
+}
+
+index = 0;
+while (index < 8)
+{
+    int array = A[index];
+    Console.Write(array + " ");
+    index++;
+}
