9927e5cc601a05df25148f05261d12915978df41
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-15 19:15:54 +0300

    add task33 - сумма массива

---
+++ task33/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задать массив из 12 элементов, заполненных числами из [0,9]. 
+//Найти сумму положительных/отрицательных элементов массива
+
+int [] A = new int [12];
+int index = 0;
+
+while (index < 12)
+{
+    A [index] = new Random().Next(0, 9); 
+    index++;
+}
+
+index = 0;
+while (index < 12)
+{
+    int array = A[index];
+    Console.Write(array + " ");
+    index++;
+}
+Console.WriteLine(" ");
+
+int sum = 0;
+for (int i=0; i< 12; i++)
+{
+    sum +=A[i];
+}
+Console.WriteLine(sum);
