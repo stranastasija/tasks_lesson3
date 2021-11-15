d65308b569cc6c1dab2271b1ec7bd90a2f0f4e9b
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-15 18:44:27 +0300

    add task30 - четные кубы

---
+++ task30/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+string cube (int A)
+{
+    string N = string.Empty;
+    for (int count = 1; count < A; count++)
+    {
+        int B = count*count*count;
+        if (B%2==0)
+        {
+            N = N + B + " ";
+        }
+    }
+    return N;
+}
+Console.WriteLine(cube(12));
