a0a10ac53d2b94efacd29cfa040cec80538ece2e
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-15 18:57:10 +0300

    add task31 - массив 8 элементов

---
+++ task31/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задать массив из 8 элементов и вывести их на экран 
+
+int [] A = new int [8];
+int index = 0;
+
+while (index < 8)
+{
+    A [index] = new Random().Next(-100, 100);
+    index++;
+}
+
+index = 0;
+while (index < 8)
+{
+    int array = A[index];
+    Console.Write(array + " ");
+    index++;
+}
