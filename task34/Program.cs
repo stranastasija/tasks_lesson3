bda90246e94372a089cea39a39061d26d8d12a60
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-18 21:40:19 +0300

    add task34 - противоположные числа

---
+++ task34/Program.cs
@@ -0,0 +1,50 @@
+﻿// Написать программу замену элементов массива на противоположные
+
+int [] sozdanie (int length)
+{
+    int [] massiv = new int [length];
+    return massiv;
+}
+
+int [] zapolnenie (int [] massiv, int min, int max)
+{
+    int index = 0;
+    while (index < massiv.Length)
+    {
+        massiv [index] = new Random ().Next (min, max +1);
+        index++;
+    }
+    return massiv;
+}
+
+string print (int [] massiv)
+{
+    string res = String.Empty;
+    int index = 0;
+    while (index < massiv.Length)
+    {
+        int array = massiv [index];
+        Console.Write (array + " ");
+        index++;
+    }
+    return res;
+}
+
+string zamena (int [] massiv)
+{
+    string res = String.Empty;
+    int index = 0;
+    while (index < massiv.Length)
+    {
+        int arr = massiv [index] * (-1);
+        Console.Write (arr + " ");
+        index++;
+    }
+    return res;
+}
+
+int [] A = sozdanie(12);
+zapolnenie(A, -10, 10);
+print (A);
+Console.WriteLine("");
+zamena (A);
