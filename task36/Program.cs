95929441139f7ccefc81d873a162f96dfc93dbf0
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-22 23:00:09 +0300

    add task 36 - массив

---
+++ task36/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. 
+//Показать количество нечетных\четных чисел
+
+int [] sozdanie (int length)
+{
+    int [] mass = new int [length];
+    return mass;
+}
+
+int [] zapolnenie (int [] mass, int min, int max)
+{
+    int i = 0;
+    while (i < mass.Length)
+    {
+        mass [i] = new Random().Next(min, max+1);
+        if (min < 100 || max > 999)
+        {
+            Console.WriteLine("Числа должны быть трехзначные");
+            break;
+        }
+        i++;
+    }
+    return mass;
+}
+
+string print (int [] mass)
+{
+    string res = String.Empty;
+    int i = 0;
+    while (i < mass.Length)
+    {
+        int arr = mass [i];
+        Console.Write(arr + " ");
+        i++;
+    }
+    return res;
+}
+
+void number (int [] mas)
+{
+    for (int i = 0; i<=mas.Length; i++)
+    {
+        if (mas [i] %2 ==0)
+        { 
+            int firstsum = 1;
+            int numbers = mas [i];
+            Console.WriteLine(numbers + " - число четное");
+            firstsum += i;
+            Console.WriteLine(firstsum + " четных чисел");
+        }
+  
+        else
+        {
+            int secondsum = 1;
+            int numbers = mas [i];
+            Console.WriteLine(numbers + " - число нечетное");
+            secondsum += i;
+            Console.WriteLine(secondsum + " нечетных чисел");
+        }
+    }
+}
+
+Console.WriteLine("Введите количество элементов для создания массива");
+string element = Console.ReadLine();
+int massiv = Convert.ToInt32 (element);
+int [] mas = sozdanie (massiv);
+Console.WriteLine("Введите минимальный порог для создания массива");
+string A = Console.ReadLine();
+int a = Convert.ToInt32 (A);
+Console.WriteLine("Введите максимальный порог для создания массива");
+string B = Console.ReadLine();
+int b = Convert.ToInt32 (B);
+Console.WriteLine("Массив: ");
+zapolnenie(mas, a, b);
+print (mas);
+Console.WriteLine("");
+number (mas);
+
