699d26bcf0bab3ce1fc4f6b9a9b1f5979d080b65
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-26 23:31:45 +0300

    add task41 - стороны треугольника

---
+++ task41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Выяснить являются ли три числа сторонами треугольника 
+
+int firstNumber = -5;
+int secondNumber = 20;
+int thirdNhumber = 18;
+
+bool tr = ((firstNumber + secondNumber > thirdNhumber) && (firstNumber + thirdNhumber > secondNumber) && (secondNumber + thirdNhumber > firstNumber));
+
+if (tr)
+{
+    Console.WriteLine("Являются");
+}
+else 
+{
+    Console.WriteLine("Не являются");
+}
+
+bool number (int firstNumber, int secondNumber, int thirdNhumber)
+{
+    if ((firstNumber + secondNumber > thirdNhumber) && (firstNumber + thirdNhumber > secondNumber) && (secondNumber + thirdNhumber > firstNumber))
+    {
+        Console.WriteLine("Являются");
+        return true;
+    }
+    else 
+    {
+        Console.WriteLine("Не являются");
+        return false;
+    }
+}
+
+Console.WriteLine(number(1,92,69));
