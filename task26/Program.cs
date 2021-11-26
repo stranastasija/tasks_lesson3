c319ce4cf90effc01456db948c11741a8ea0281f
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-26 23:54:52 +0300

    add task26 - степень числа

---
+++ task26/Program.cs
@@ -0,0 +1,22 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+void number (int A, int B)
+{
+    string stepen = string.Empty;
+    
+    int StepenNumber = Convert.ToInt32(stepen);
+
+    while (A != 0) 
+    {
+        if (B == 0)
+        {
+            StepenNumber = 1;
+        }
+        else
+        {
+            StepenNumber = A ^ B;
+        }
+    }
+    
+}
+number(2,3);
