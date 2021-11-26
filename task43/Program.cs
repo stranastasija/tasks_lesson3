1292e75ec46ace8308b7666015cc4160f12535ff
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-26 23:39:01 +0300

    add task43 - перевод числа

---
+++ task43/Program.cs
@@ -0,0 +1,24 @@
+﻿// Написать программу преобразования десятичного числа в двоичное
+
+string programm (int a)
+{
+    string number = string.Empty;
+
+    while (a!=0)
+    {
+        if (a%2==0)
+        {
+            number +=$"{0}";
+        }
+        else
+        {
+            number +=$"{1}";
+        }
+
+        a = a/2;
+    }
+
+    return number;
+}
+
+Console.WriteLine(programm(69));
