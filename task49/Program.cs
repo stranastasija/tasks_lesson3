aa2a16283ad43e3c0f566f4532288490d9578d38
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-29 22:04:52 +0300

    add task49 - двумерный массив с вещественными числами

---
+++ task49/Program.cs
@@ -0,0 +1,33 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+void Print(double [,] mass)
+{
+
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            Console.Write($"{mass[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+void Fill (double [,] mass)
+{
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            mass [i,j] = new Random().Next (-10, 10);
+        }
+    
+    }
+}
+
+double [,] massiv = new double [3, 6];
+//Print(massiv);
+//Console.WriteLine();
+Fill(massiv);
+Print(massiv);
