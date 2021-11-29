b2d37d5d60ef6bfb0c58bf48fe8b4687b30b5e3f
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-29 21:59:29 +0300

    add task48 - двумерные массивы

---
+++ task48/Program.cs
@@ -0,0 +1,34 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+
+
+void Print(int [,] mass)
+{
+
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            Console.Write($"{mass[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+void Fill (int [,] mass)
+{
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            mass [i,j] = new Random().Next (1, 10);
+        }
+    
+    }
+}
+
+int [,] massiv = new int [3, 6];
+Print(massiv);
+Console.WriteLine();
+Fill(massiv);
+Print(massiv);
