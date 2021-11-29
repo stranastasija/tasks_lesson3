9b2dc80e291f787edf34269e88bf0f767d4829a1
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-29 14:49:32 +0300

    add task45 - числа Фибоначчи

---
+++ task45/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать числа Фибоначчи
+
+Console.WriteLine ("На каком числе вы хотите остановиться? Введите его:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstNumber = 1;
+int secondNumber = 1;
+Console.Write("{0}", firstNumber + " ");
+Console.Write("{0}", secondNumber + " ");
+
+int summa = 0;
+while (number >= summa)
+{
+    summa = firstNumber + secondNumber;
+    Console.Write("{0}", summa + " ");
+    firstNumber = secondNumber;
+    secondNumber = summa;
+}
