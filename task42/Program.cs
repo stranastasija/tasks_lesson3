4a7fffab9699a6399544d8d96ede31cf8384c6c2
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-29 14:29:28 +0300

    add task42 - числа больше 0

---
+++ task42/Program.cs
@@ -0,0 +1,16 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры
+
+Console.WriteLine("Введите любое количество цифр через пробел");
+string Number = Console.ReadLine ();
+Console.WriteLine ("Вы ввели следующие числа:");
+Console.Write (Number + " ");
+
+int chislo = Convert.ToInt32(Number);
+
+int kolichestvo = 0;
+
+while (chislo > 0)
+{
+    kolichestvo = kolichestvo + 1;
+}
+Console.WriteLine("Вы ввели " + kolichestvo + " количество чисел больше нуля");
