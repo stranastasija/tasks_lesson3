3d003960682647407c69f3e723675937c467aa87
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-29 16:00:22 +0300

    add task47 - клон

---
+++ task47/Program.cs
@@ -0,0 +1,24 @@
+﻿// Написать программу копирования массива
+
+namespace Copy 
+{
+    class Programm
+    {
+        static void Main (string [] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int [] array = new int [a];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array [i] = int.Parse(Console.ReadLine());
+
+            }
+            int [] array1 = (int []) array.Clone();
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.Write(array1[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
