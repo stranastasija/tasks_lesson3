68a074c35f398913aeb3fa43eb023d8eb46cace4
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-30 19:09:34 +0300

    add task54 - сумма горизонтали

---
+++ task54/Program.cs
@@ -0,0 +1,52 @@
+﻿// В матрице чисел найти сумму элементов главной диагонали
+
+void Print(int [,] mass)
+{
+
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            Console.Write($"{mass[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+void Fill (int [,] mass)
+{
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            mass [i,j] = new Random().Next (0, 10);
+        }
+    
+    }
+}
+
+void Summa (int [,] mass)
+{
+    int sum = 0;
+
+    for (int i = 0; i < mass.GetLength(0); i++)
+    { 
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            sum = mass[0,0] + mass[1,1] + mass[2,2];   
+            Console.Write(sum + " ");
+            break;
+        }
+        break;
+    }
+}
+
+
+int [,] matr = new int [3, 3];
+
+Fill (matr);
+Print (matr);
+Console.WriteLine();
+Summa(matr);
+ 
