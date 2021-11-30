06e657504b3725272148bf347a84ef9dadf6dca7
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-30 18:11:52 +0300

    add task51 - a m+n

---
+++ task51/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void Print(double [,] mass)
+{
+
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            Console.Write($"{mass[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+void Fill (double [,] mass)
+{
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            mass [i,j] = i+j;
+        }
+    
+    }
+}
+
+double [,] matrix = new double [4, 3];
+Print (matrix);
+Console.WriteLine();
+Fill (matrix);
+Print(matrix);
