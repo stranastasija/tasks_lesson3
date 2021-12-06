ac2a78f552ff4aa8503ebf1f5e59f57f94d1b495
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-12-06 18:59:22 +0300

    add task64 - треугольник

---
+++ task64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать треугольник Паскаля
+
+static int Tr (int n)
+{
+    int [] a = new int [n];
+    a[0] = 1;
+    for (int i=1; i<n; i++)
+    {
+        int left = 0;
+        for (int j = 0; j <=i; j++)
+        {
+            int treu = a [j];
+            a [j] += left;
+            left = treu;
+        }
+    }
+    return a;
+}
+Console.WriteLine(Tr(10));
