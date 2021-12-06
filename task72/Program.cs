e34567a31f2ee7e5253d1f84e0652a046dd9a242
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-12-06 21:45:52 +0300

    add task72 - степень

---
+++ task72/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+int Count = 0;
+int res = 1;
+int Pow (int A, int B)
+{
+    if (B == 0) return 1;
+    if (Count < B) Pow (A, B-1);
+    res *= A;
+    return res;
+}
+
+Console.WriteLine(Pow(3,3));
