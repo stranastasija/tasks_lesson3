bb9d29bc4b53841f98373ddb9543f0d07b4c0727
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-12-06 21:13:56 +0300

    new file

--- task67/Program.cs
+++ task67/Program.cs
@@ -4,7 +4,7 @@ string Number (int n)
 {
     int m = 1;
     if (n == m) return $"{n}";
-    return (m > n) ? Number (n) + " " + (n-1):
-    Number (n) + " " + (n-1);
+    return (n > 1) ? Number (n) + " " + (n-1):
+    
 }
 Console.WriteLine(Number(10));
