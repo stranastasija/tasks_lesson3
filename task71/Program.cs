bdbc729860ae691bf2f69b2466aa6b96449ee63d
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-12-06 21:11:38 +0300

    add task71 - функция Аккурмана

---
+++ task71/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления функции Аккермана
+
+static int A(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return A(n - 1, 1);
+    else
+      return A(n - 1, A(n, m - 1));
+}
+Console.WriteLine(A(3,7));
