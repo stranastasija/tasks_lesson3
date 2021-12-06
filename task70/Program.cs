afa8fdd72348925e277bd10a8a4936bc73b3b9ae
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-12-06 18:29:46 +0300

    add task70 - сумма в числе

---
+++ task70/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти сумму цифр числа
+
+int sum = 0;
+int SummaNumber (int number)
+{
+    if (number == 0) return sum;
+    else
+    {
+        sum += number % 10;
+        number = SummaNumber(number / 10);
+    }
+    return sum;
+}
+
+Console.WriteLine (SummaNumber(492));
