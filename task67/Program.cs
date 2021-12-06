e9324725e6282a722f24202bce9f534ab2f72c69
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-12-06 18:46:05 +0300

    add task67 - натуральные

---
+++ task67/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать натуральные числа от N до 1, N задано
+
+string Number (int n)
+{
+    int m = 1;
+    if (n == m) return $"{n}";
+    return (m > n) ? Number (n) + " " + (n-1):
+    Number (n) + " " + (n-1);
+}
+Console.WriteLine(Number(10));
