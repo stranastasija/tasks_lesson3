6186bcf9d32779dad2c462812d7fd318736658f1
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-12-08 22:41:46 +0300

    add task74 - alphabet

---
+++ task74/Program.cs
@@ -0,0 +1,17 @@
+﻿// В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». 
+//Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита
+
+void FindWords(string alphabet, char [] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine ($"{n++} {new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length +1);
+    }
+}
+
+FindWords("аисв", new char [3]);
